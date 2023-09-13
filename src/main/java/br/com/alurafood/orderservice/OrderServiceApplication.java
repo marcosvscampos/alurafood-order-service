6f1d34107d5d25d3459abb18a24dd94523b5c9ee
Marcos Campos <contato.marcoscampos@gmail.com>
2023-09-13 15:05:37 -0300

    Criação de imagem do docker

--- src/main/java/br/com/alurafood/orderservice/OrderServiceApplication.java
+++ src/main/java/br/com/alurafood/orderservice/OrderServiceApplication.java
@@ -2,9 +2,8 @@ package br.com.alurafood.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderServiceApplication {
 
